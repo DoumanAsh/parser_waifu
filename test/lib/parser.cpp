b28b661f9013403dab8b72b0597949df61f64d4c
Douman <douman@gmx.se>
2017-08-06 08:29:26 +0300

    parser::internal tests

--- test/lib/parser.cpp
+++ test/lib/parser.cpp
@@ -1,13 +1,19 @@
 #include "parser/parser.hpp"
-using namespace parser;
+#include "parser/internal.hpp"
 
 #include "gtest/gtest.h"
 using namespace ::testing;
 
 TEST(LibParser, MecabParseWorks) {
-    Mecab mecab;
+    parser::Mecab mecab;
     //Uses char codes for "tekito" to avoid damn VC++ codepage trouble
     const std::string str(u8"\u624B\u5143");
 
     mecab.parse(str);
+}
+
+TEST(LibParser, should_escape_html) {
+    const auto escaped = parser::internal::escape_html(">>< &' test \"");
+
+    ASSERT_STREQ(escaped.c_str(), "&gt;&gt;&lt; &amp;&apos; test &quot;");
 }
